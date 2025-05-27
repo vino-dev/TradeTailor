50f0d950ba3de9a2e4dc4858b4100bfd2e222d6b
vino-dev <vinosekar210@gmail.com>
2025-05-27 15:35:54 +0530

    commit

---
+++ TradeTailor/src/main/java/com/TradeTailor/TradeTailor/TradeTailorApplication.java
@@ -0,0 +1,13 @@
+package com.TradeTailor.TradeTailor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeTailorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeTailorApplication.class, args);
+	}
+
+}
